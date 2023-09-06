a85d97fdb1f9cbc318f308789b91c5f34f32799b
YeslhanOrtiz <104031910+YeslhanOrtiz@users.noreply.github.com>
2023-09-06 12:14:09 -0500

    Nuevo producto

--- app/products/forms.py
+++ app/products/forms.py
@@ -1,9 +1,31 @@
 #Importar dependencias
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import InputRequired, NumberRange
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 #Formulario de registro de nuevo producto
 class NewProductForm(FlaskForm):
-    nombre = StringField('Ingrese nombre:')
-    precio = StringField('Ingrese precio: ')
+    nombre = StringField(validators = [ InputRequired(message="El campo nombre esta vacío.") ],
+                        label = 'Ingrese nombre:')
+    precio = IntegerField(validators=[
+                                        InputRequired(
+                                            message= "Precio Requerido"),
+                                        NumberRange(
+                                            message= " Precio fuera de rango", 
+                                            min = 1000,
+                                            max = 10000
+                                        )
+                                    ],
+                        label = 'Ingrese precio:')
+    imagen = FileField(label = 'Cargue imagen del producto:',
+                        validators =[
+                            FileRequired(
+                                message=" Suba una imagen"
+                                ),
+                            FileAllowed(
+                                ["jpg" , "png"], 
+                                message="El tipo de archivo es incorrecto"
+                            )
+                        ])
     submit = SubmitField ('Registrar')
--- app/products/routes.py
+++ app/products/routes.py
@@ -1,6 +1,27 @@
 from flask import render_template 
 from . import products
+from .forms import NewProductForm
+import app
+import os 
 
-@products.route('/create')
+@products.route('/create', methods=['GET', 'POST'])
 def crear_producto():
-    return render_template('new.html')
+    p = app.models.Producto()
+    form = NewProductForm()
+    if form.validate_on_submit():
+        #llenar atributos
+        #del objeto producto
+        #con el formulario
+        form.populate_obj(p)
+        #regitrar producto en bd
+        app.db.session.add(p)
+        p.imagen = form.imagen.data.filename
+        app.db.session.commit()
+        
+        #trasladar la imagen cargada a la carpeta
+        #app/prodcutos/imagenes
+        archivo = form.imagen.data
+        archivo.save( os.path.abspath(os.getcwd() + '/app/products/imagenes/' + p.imagen) )
+        
+        return os.getcwd()
+    return render_template('new.html', form = form)
